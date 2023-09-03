c3c80e05d0dd4e11bca6bca70819276ea7fdf84d
Kristiyan <kris.bakalov98@gmail.com>
2023-09-03 12:38:01 +0300

    ab-switcher

---
+++ tab-switcher/script.js
@@ -0,0 +1,16 @@
+function openTab(evt, tabName) {
+  var i, tabContent, tabButton;
+  
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  tabButton = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButton.length; i++) {
+    tabButton[i].className = tabButton[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
